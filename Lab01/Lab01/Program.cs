afb2c644ac8ac4eb6789876af84c39aebca907fc
chitsanu345 <chitsanu345@gmail.com>
2017-09-07 19:42:47 +0700

    P03-1

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is text 1.");
+            Console.WriteLine("This is text 2.");
+            Console.WriteLine("This is text 3.");
+
         }
     }
 }
